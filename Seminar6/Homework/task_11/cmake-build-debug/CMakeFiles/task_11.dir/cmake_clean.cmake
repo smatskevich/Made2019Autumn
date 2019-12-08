55a1c64c23a8788ac9c1d6264b943d10a489f187
IskanderN <iskander.sych@gmail.com>
2019-12-08 23:31:06 +0300

    task_11

---
+++ Seminar6/Homework/task_11/cmake-build-debug/CMakeFiles/task_11.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/task_11.dir/main.cpp.o"
+  "task_11"
+  "task_11.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/task_11.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Seminar6/Homework/task_11/main.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+#include <vector>
+#include <deque>
+
+
+// Узел односвязного списка
+struct QueueNode {
+  int data;
+  QueueNode* next;
+};
+
+class Queue {
+ public:
+  ~Queue();
+
+  // Проверка очереди на пустоту
+  bool empty() const;
+  // Добавление элемента
+  void push(int value);
+  QueueNode* head = nullptr;
+ private:
+  QueueNode* tail = nullptr;
+};
+
+Queue::~Queue() {
+  while(head) {
+    QueueNode* to_delete = head;
+    head = head->next;
+    delete to_delete;
+  }
+}
+
+bool Queue::empty() const {
+  return head == nullptr;
+}
+
+void Queue::push(int value) {
+  if (empty()) {
+    head = tail = new QueueNode{value, nullptr};
+  } else {
+    tail->next = new QueueNode{value, nullptr};
+    tail = tail->next;
+  }
+}
+
+
+class Graph {
+ private:
+  std::vector<Queue*> nodes = {new Queue()};
+
+ public:
+  void add_edge(int start, int end);
+  int count_routes(int start, int end);
+};
+
+
+void Graph::add_edge(int start, int end) {
+  while (nodes.size() < start + 1) {
+    Queue *new_list = new Queue();
+    nodes.push_back(new_list);
+  }
+
+  while (nodes.size() < end + 1) {
+    Queue *new_list = new Queue();
+    nodes.push_back(new_list);
+  }
+  nodes[start]->push(end);
+  nodes[end]->push(start);
+}
+
+int Graph::count_routes(int start, int end) {
+  std::deque<int> queue;
+  std::vector<std::pair<int, int>> routes;
+  int element;
+  for (int i=0; i<nodes.size(); i++) {
+    routes.push_back(std::make_pair(0, nodes.size() + 1));
+  }
+  routes[start] = std::make_pair(1, 0);
+  queue.push_back(start);
+  while(!queue.empty()) {
+    element = queue.front();
+    queue.pop_front();
+    if (element == end) return routes[end].first;
+    QueueNode* edge = nodes[element]->head;
+    while (edge) {
+      if (routes[edge->data].second == nodes.size() + 1) {
+        queue.push_back(edge->data);
+      }
+      if (!(routes[edge->data].second <= routes[element].second)) {
+        routes[edge->data].first += routes[element].first;
+        routes[edge->data].second = routes[element].second + 1;
+      }
+      edge = edge->next;
+    }
+  }
+  return 0;
+}
+
+
+int main() {
+  /*
+  v: кол-во вершин (макс. 50000),
+  n: кол-во ребер(макс. 200000),
+  n пар реберных вершин,
+  пара вершин (u, w) для запроса.
+  */
+  int v;
+  int n;
+  int start;
+  int end;
+  std::cin >> v >> n;
+  Graph graph;
+
+  for (int i=0; i<n; i++) {
+    std::cin>>start>>end;
+    graph.add_edge(start, end);
+  }
+  std::cin>>start>>end;
+  std::cout<<graph.count_routes(start, end);
+  return 0;
+
+}
